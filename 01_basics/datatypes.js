af9d9c4ba7c4c83c6c7d6a2c49546081c5b8a309
iaastha22 <168904731+iaastha22@users.noreply.github.com>
2024-11-08 10:57:29 +0000

    varibles n datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all jS code as newer version
+
+//alert("hello") we are using nodejs not brower
+
+//code readability should be high
+
+let name ="krrish"     //string
+let age = 20           //num
+let isLoggedIn =false       //boolean
+
+
+
+// number => 2 to power 53
+//bigint 
+//String  =>""
+//boolean => true/false
+//null =>standlone value/ indicates absence of object
+//undefined =>absence of value
+//symbol =>unique 
+
+
+
+
+//objects
+
+console.log(typeof null);
---
+++ 01_basics/varible.js
@@ -0,0 +1,18 @@
+const  accountId = 15423
+let accountEmail = "krrish@google.com"
+var accountPassword = "12387"
+accountCity = "jaipur"
+
+//accountId  = 2//not allowed to change const varible 
+
+accountEmail = "kb@k.com" 
+accountPassword = "25252225"
+accountCity = "nodia"
+
+
+console.log(accountId);
+
+console.table([accountId,accountCity,accountPassword]);
+
+/*
+don't prefer using var as it changes the actual info  it also block scope n functional scope */
